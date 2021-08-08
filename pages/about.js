7159fae2b1d3ee7471a525dc5b01433357852c99
G <gaston.garcia@gmail.com>
2021-08-07 20:05:41 -0600

    Adding first basic page structure

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is an Events and Event Listing app for musicians and artists.</p>
+        </div>
+    )
+}
+
+export default AboutPage

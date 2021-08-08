3327d322e0cdd3402bb3c1800c93eb19c1fca39b
G <gaston.garcia@gmail.com>
2021-08-07 20:14:10 -0600

    Added tailwind

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 const AboutPage = () => {
     return (
-        <div>
+        <div className="w-8/12 mx-auto mt-10">
             <h1>About Page</h1>
             <p>This is an Events and Event Listing app for musicians and artists.</p>
         </div>

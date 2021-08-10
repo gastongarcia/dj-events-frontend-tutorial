da8110396cd4516ab86415d7479fcbf89aa3bffd
G <gaston.garcia@gmail.com>
2021-08-10 14:02:25 -0600

    Added navigation

---
+++ components/Header.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Link from "next/link";
+const Header = () => {
+  const [isExpanded, toggleExpansion] = useState(false);
+  const [isShown, toggleShown] = useState(false);
+
+  return (
+    <nav className="w-full flex justify-between flex-wrap p-6 lg:pl-0 bg-gray-100">
+      <div className="block lg:hidden">
+        <button
+          className="flex px-3 py-2 border rounded text-gray-800 border-gray-400 hover:text-black hover:underline hover:border-black"
+          onClick={() => toggleExpansion(!isExpanded)}
+        >
+          Menu
+        </button>
+      </div>
+      <div
+        className={`${
+          isExpanded ? `block` : `hidden`
+        } w-full block flex-grow lg:flex lg:items-center lg:w-auto nav-links transition duration-400 ease-in-out transform`}
+      >
+        <div className="lg:flex-grow md:items-center lg:items-center ml-10 pt-3 tracking-tight">
+          <div className="mt-4 lg:inline-block lg:mt-0 text-gray-600 text-lg hover:text-black hover:underline mr-8">
+            <Link href="/" activeClassName="underline">
+              Home
+            </Link>
+          </div>
+
+          <div className="mt-4 lg:inline-block lg:mt-0 text-gray-600 text-lg hover:text-black hover:underline mr-8">
+            <Link href="/events" activeClassName="underline">
+              Events
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,24 @@
-import Head from "next/head"
+import Head from "next/head";
+import Header from "./Header";
 
-const Layout = ({title,description,keywords, children}) => {
-    return (
-        <div>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={description}/>
-                <meta name="keywords" content={keywords}/>
-            </Head>
-            <div className="main">
-            {children}
-            </div>
-            
-        </div>
-    )
-}
+const Layout = ({ title, description, keywords, children }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <Header />
+      <div className="main">{children}</div>
+    </div>
+  );
+};
 
 Layout.defaultProps = {
-    title: "Event Listings",
-    description: "An app with music events and event management",
-    keywords:"Music app, Event management, Events online"
-}
+  title: "Event Listings",
+  description: "An app with music events and event management",
+  keywords: "Music app, Event management, Events online",
+};
 
-export default Layout
-
+export default Layout;
